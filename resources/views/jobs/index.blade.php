df6e57472eeb6ed3ba4e0f0c25564c14da4c4926
thami88 <thaminduliyanage97@gmail.com>
2024-06-01 13:41:08 +0530

    build the general architecture for a job platform, including employers, tagging, and more

--- resources/views/jobs/index.blade.php
+++ resources/views/jobs/index.blade.php
@@ -13,9 +13,9 @@
                 Featured Jobs
             </x-section-heading>
              <div class="grid lg:grid-cols-3 gap-8 mt-6">
-                <x-job-card/>
-                <x-job-card/>
-                <x-job-card/>
+                @foreach ($featuredJobs as $job)
+                    <x-job-card :$job/>
+                @endforeach
              </div>
         </section>
         <section>
@@ -23,17 +23,9 @@
                 Tags
             </x-section-heading>
             <div class="mt-6 space-x-1">
-                <x-tag>Tag</x-tag>
-                <x-tag>Tag</x-tag>
-                <x-tag>Tag</x-tag>
-                <x-tag>Tag</x-tag>
-                <x-tag>Tag</x-tag>
-                <x-tag>Tag</x-tag>
-                <x-tag>Tag</x-tag>
-                <x-tag>Tag</x-tag>
-                <x-tag>Tag</x-tag>
-                <x-tag>Tag</x-tag>
-                <x-tag>Tag</x-tag>
+                @foreach ($tags as $tag)
+                    <x-tag :$tag />
+                @endforeach
             </div>
         </section>
         <section>
@@ -41,9 +33,9 @@
                 Recent Jobs
             </x-section-heading>
             <div class="mt-6 space-y-6">
-                <x-job-card-wide />
-                <x-job-card-wide />
-                <x-job-card-wide />
+                @foreach ($jobs as $job)
+                    <x-job-card-wide :$job/>
+                @endforeach
             </div>
         </section>
     </div>
